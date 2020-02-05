75e7ce344af63be6f87acc0464a3459773b1299e
Rhea Premanand <rheapremanand@Rheas-MacBook-Pro-2.local>
2020-02-05 14:46:10 -0500

    Initial commit

---
+++ app/src/main/java/com/premanandrhea/quizapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.premanandrhea.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button submitButton
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
+        submitButton.setOnClick
+    }
+}
